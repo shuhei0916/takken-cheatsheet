dc6d1b713924a596e12ea4642c1e7124be52861b
shuhei0916 <shuheiito.stc@gmail.com>
2024-08-30 14:47:03 +0900

    Modify takken-scraper.py

--- takken-scraper.py
+++ takken-scraper.py
@@ -1,14 +1,24 @@
 
 import requests, bs4
 
+
+# html = 
 res = requests.get('https://www.google.com/')
 res.raise_for_status()
 print(type(res))
 
 # print(res.text)
-soup = bs4.BeautifulSoup(res.text, features="html.parser")
-hoge = soup.select('div')
-print(hoge)
+# soup = bs4.BeautifulSoup(res.text, features="html.parser")
+# hoge = soup.select('div')
+# print(hoge)
 
-hehe = soup.select('#gbqfbb')
-print('hehe', hehe)
+# hehe = soup.select('#gbqfbb')
+# print('hehe', hehe)
+
+with open('./sample-html.html') as f:
+    print(type(f))
+    example_soup = bs4.BeautifulSoup(f, features="html.parser")
+    elems = example_soup.select('#author')
+    print(type(elems))
+    print(elems[0])
+    print(elems[0].getText())
