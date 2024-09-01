16a08d194cd732c1eb4a7a04cd57c19661cb5409
shuhei0916 <shuheiito.stc@gmail.com>
2024-09-02 08:26:02 +0900

    Add selenium_basic.py

---
+++ selenium_basic.py
@@ -0,0 +1,30 @@
+# seleniumの必要なライブラリをインポート
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# tkinter（メッセージボックス表示）の必要なライブラリをインポート
+import tkinter
+from tkinter import messagebox
+
+# Chrome Webドライバー の インスタンスを生成
+driver = webdriver.Chrome() 
+
+# WebドライバーでQiitaログインページを起動
+driver.get('https://www.selenium.dev/selenium/web/web-form.html')
+
+title = driver.title
+print(title) 
+
+text_box = driver.find_element(by=By.NAME, value= 'my-text')
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value='button')
+
+text_box.send_keys('Selenium')
+driver.implicitly_wait(1)
+submit_button.click()
+driver.implicitly_wait(10)
+
+message = driver.find_element(by=By.ID, value='message')
+text = message.text
+print(text)
+
+driver.quit()
