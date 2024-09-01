7e0a155af29070543a9792c40057cab9c32908e1
shuhei0916 <shuheiito.stc@gmail.com>
2024-09-02 01:11:43 +0900

    Split to 2 tests and Make it green

--- takken_scraper.py
+++ takken_scraper.py
@@ -3,40 +3,38 @@ import requests, bs4
 
 
 def scrape_question(soup):
+    question_section = soup.find('section', class_='content mondai')
+    
+    question_number = question_section.find('h3').get_text().strip('問')
+    
     question = "次の1から4までの記述のうち、民法の規定、判例及び下記判決文によれば、誤っているものはどれか。"
-    number = "1"
-    return question, number 
+    # number = "1"
+    return question, question_number 
 
 def main():
-    # html = 
     res = requests.get('https://takken-siken.com/kakomon/2023/01.html')
     res.raise_for_status()
-    # print(type(res))
-
-    # print(res.text)
-    soup = bs4.BeautifulSoup(res.text, features="html.parser")
+    soup = bs4.BeautifulSoup(res.text, features='html.parser')
     
+    # question_section = soup.find('section', class_='content mondai')
+    # if question_section:
+    #     question_text = question_section.get_text(strip=True)
+    #     print(question_text)
+    # else:
+    #     print("couldnt find question section")
+        
+    # print(question_section.find('h3'))
     
-    question_section = soup.find('section', class_='content mondai')
-    if question_section:
-        question_text = question_section.get_text(strip=True)
-        print(question_text)
-    else:
-        print("couldnt find question section")
-    # print(type(soup))
-    # hoge = soup.select('div')
-    # print(hoge)
-
-    # hehe = soup.select('#gbqfbb')
-    # print('hehe', hehe)
-
-    # with open('./sample-html.html') as f:
-    #     print(type(f))
-    #     example_soup = bs4.BeautifulSoup(f, features="html.parser")
-    #     elems = example_soup.select('#author')
-    #     print(type(elems))
-    #     print(elems[0])
-    #     print(elems[0].getText())
+        # 問題番号を含むh3タグを取得
+    ques_num_element = soup.find('h3')
+    ques_num = ques_num_element.get_text(strip=True)
+    
+    # 問題番号と問題文を分離
+    number = ques_num.replace('問', '').strip()  # "問1" から "1" を抽出
+    # question = ques_num_element.find_next('div', class_='content mondai').get_text(strip=True).replace(ques_num, '').strip()
+    
+    # print(question)
+    print(number)
 
 if __name__ == "__main__":
     main()
--- test_takken_scraper.py
+++ test_takken_scraper.py
@@ -10,16 +10,18 @@ class TestTakkenScraper(unittest.TestCase):
         with open('sample.html', 'r', encoding='utf-8') as f:
             self.html_content = f.read()
         
-    # def testfail(self):
-    #     self.fail("this should fail")
-    
     def test_question_extraction(self):
         soup = BeautifulSoup(self.html_content, 'html.parser')
-        question, number = scrape_question(soup)
-        expected_question = "次の1から4までの記述のうち、民法の規定、判例及び下記判決文によれば、誤っているものはどれか。"
-        expected_number = "1"
+        question, _ = scrape_question(soup)
         
+        expected_question = "次の1から4までの記述のうち、民法の規定、判例及び下記判決文によれば、誤っているものはどれか。"
         self.assertEqual(question, expected_question)
+
+    def test_number_extraction(self):
+        soup = BeautifulSoup(self.html_content, 'html.parser')
+        _, number = scrape_question(soup)
+        
+        expected_number = "1"
         self.assertEqual(number, expected_number)
         
     
