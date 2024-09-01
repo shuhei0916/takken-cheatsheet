37e00a12a73f63b7dd0c950702aab16cc5d2a8d6
shuhei0916 <shuheiito.stc@gmail.com>
2024-09-02 03:36:26 +0900

    Add scrape_year()

--- takken_scraper.py
+++ takken_scraper.py
@@ -1,5 +1,6 @@
 
 import requests, bs4
+import re
 
 
 def scrape_question(soup):
@@ -10,6 +11,13 @@ def scrape_question(soup):
     question_number = question_number.strip('問')
     
     return question, question_number 
+
+def scrape_year(soup): # NOTE: URLから年度を取得するようにしても良いかも
+    year_text = soup.find('h2').get_text()
+    match = re.search(r'(令和|平成)\d+年', year_text) # NOTE: 平成と令和以外対応していないので注意
+    if match:
+        return match.group(0)
+    return None
 
 def main():
     res = requests.get('https://takken-siken.com/kakomon/2023/01.html')
--- test_takken_scraper.py
+++ test_takken_scraper.py
@@ -1,6 +1,6 @@
 import unittest
 # from unittest.mock import MagicMock
-from takken_scraper import scrape_question
+from takken_scraper import scrape_question, scrape_year
 from bs4 import BeautifulSoup
 
 
@@ -8,21 +8,24 @@ class TestTakkenScraper(unittest.TestCase):
     def setUp(self):
         # テスト用のHTMLファイルを読み込む
         with open('sample.html', 'r', encoding='utf-8') as f:
-            self.html_content = f.read()
+            self.html_content = f.read() 
+        self.soup = BeautifulSoup(self.html_content, 'html.parser')
+            
         
     def test_question_extraction(self):
-        soup = BeautifulSoup(self.html_content, 'html.parser')
-        question, _ = scrape_question(soup)
-        
+        question, _ = scrape_question(self.soup)
         expected_question = "相隣関係に関する次の記述のうち、民法の規定によれば、正しいものはどれか。"
         self.assertEqual(question, expected_question)
 
     def test_number_extraction(self):
-        soup = BeautifulSoup(self.html_content, 'html.parser')
-        _, number = scrape_question(soup)
-        
+        _, number = scrape_question(self.soup)
         expected_number = "2"
         self.assertEqual(number, expected_number)
+        
+    def test_year_extraction(self):
+        actual = scrape_year(self.soup)
+        expected = "令和5年"
+        self.assertEqual(actual, expected)
         
     
 if __name__ == "__main__":
