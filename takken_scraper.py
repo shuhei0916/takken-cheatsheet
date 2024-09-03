c87f80a0b611c91d1fc57a02e98d5fe256ad6100
shuhei0916 <shuheiito.stc@gmail.com>
2024-09-03 11:20:48 +0900

    Modify test_takken_scraper.py

--- takken_scraper.py
+++ takken_scraper.py
@@ -4,14 +4,19 @@ import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def scrape_question():
-    return "1"
+def scrape_question(driver): 
+    driver.implicitly_wait(0.5)
+    question_elements = driver.find_elements(By.CSS_SELECTOR, "section.content")
+    return question_elements
 
 def main():
     driver = webdriver.Chrome() 
 
-    # WebドライバーでQiitaログインページを起動
-    driver.get('https://www.selenium.dev/selenium/web/web-form.html')
+    driver.get('https://takken-siken.com/marubatu.php')
+    
+    question_elements = scrape_question(driver)
+    for element in question_elements:
+        print(element.text)
         
 
 if __name__ == "__main__":
--- test_takken_scraper.py
+++ test_takken_scraper.py
@@ -9,17 +9,16 @@ from takken_scraper import scrape_question
 class TestTakkenScraper(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome() 
-        self.driver.get('https://www.selenium.dev/selenium/web/web-form.html')
+        self.driver.get('https://takken-siken.com/marubatu.php')
 
-    
-    def test_get_title(self):
-        title = self.driver.title
-        self.assertEqual(title, "Web form")
+    # def test_get_title(self):
+    #     title = self.driver.title
+    #     self.assertEqual(title, "宅建士 一問一答道場🥋｜宅建試験ドットコム")
         
     def test_question_extraction(self):        
-        actual = scrape_question()
-        expected = "1"
-        self.assertEqual(actual, expected)
+        question = scrape_question(self.driver)
+        expected = "制限行為能力者に関する次の記述のうち、民法の規定によれば、正しいか否かを答えよ。"
+        self.assertEqual(question, expected)
         
     
 if __name__ == "__main__":
