58cb3786169c4db52d01a8ab6e10930e20efb17d
shuhei0916 <shuheiito.stc@gmail.com>
2024-09-08 14:05:39 +0900

    Modify import sentence in test_takken_scraper.py

--- test_takken_scraper.py
+++ test_takken_scraper.py
@@ -11,7 +11,7 @@ import re
 from unittest.mock import mock_open, patch, MagicMock
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from takken_scraper import button_click, scrape_info, scrape_answer, scrape_kaisetsu, scrape_option_text, scrape_question_text, collect_and_write_questions_to_csv
+import takken_scraper as ts
 
 
 class TestTakkenScraper(unittest.TestCase):
@@ -21,7 +21,7 @@ class TestTakkenScraper(unittest.TestCase):
         cls.driver = webdriver.Chrome()
         cls.driver.get('https://takken-siken.com/marubatu.php')
         
-        button_click(cls.driver)
+        ts.button_click(cls.driver)
         
     @classmethod
     def tearDownClass(cls):
@@ -29,35 +29,35 @@ class TestTakkenScraper(unittest.TestCase):
         cls.driver.quit()        
     
     def test_scrape_question_text(self):
-        question = scrape_question_text(self.driver)
+        question = ts.scrape_question_text(self.driver)
         self.assertIn("答えよ。", question) # おいおい、こんなテストでいいのか笑
     
     def test_option_text(self):
-        option_text = scrape_option_text(self.driver)
+        option_text = ts.scrape_option_text(self.driver)
         self.assertIn("。", option_text) # 句読点が含まれているかどうかのみをテストしている。
     
     def test_scraper_year(self):
-        year, _, _ = scrape_info(self.driver)
+        year, _, _ = ts.scrape_info(self.driver)
         expected = r"(平成|令和)" # NOTE: 平成と令和という文字列が含まれていることのみを確認
         self.assertRegex(year, expected)
         
     def test_scrape_ques_num(self):
-        _, ques_num, _ = scrape_info(self.driver)
+        _, ques_num, _ = ts.scrape_info(self.driver)
         expected = r"問\d+"
         self.assertRegex(ques_num, expected)
         
     def test_scrape_ques_num(self):
-        _, _, opt_num = scrape_info(self.driver)
+        _, _, opt_num = ts.scrape_info(self.driver)
         expected = r"肢\d+"
         self.assertRegex(opt_num, expected)
         
     def test_scrape_answer(self):
-        answer = scrape_answer(self.driver)
+        answer = ts.scrape_answer(self.driver)
         expected = r"(正|誤)"
         self.assertRegex(answer, expected)
         
     def test_scrape_kaisetsu(self):
-        kaisetsu = scrape_kaisetsu(self.driver)
+        kaisetsu = ts.scrape_kaisetsu(self.driver)
         expected = r"(誤り|正しい|不適当|適当|違反する|違反しない)" # NOTE: 必要なテストケース全てを網羅できているわけではない
         self.assertRegex(kaisetsu, expected)
 
@@ -81,7 +81,7 @@ class TestCSVWriter(unittest.TestCase):
         # ダミーのドライバを使ってテスト
         driver = None
         
-        collect_and_write_questions_to_csv(driver, num_questions=1, filename="dummy.csv")
+        ts.collect_and_write_questions_to_csv(driver, num_questions=1, filename="dummy.csv")
 
         # 書き込まれた内容を確認
         mock_file.assert_called_once_with('dummy.csv', mode='w', newline='', encoding='utf-8')
